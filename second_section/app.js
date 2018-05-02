43c4c4530e7484aaea7c63fb77fa4e5d18dc8547
SebastianGaud <sebastiano.gaudeano@live.com>
2018-05-02 15:24:28 +0200

    design circle objects

---
+++ second_section/app.js
@@ -0,0 +1,15 @@
+
+// Object literal syntax
+const circle = {
+    // properties
+    radius: 1,
+    location: {
+        x: 2,
+        y:1
+    },
+    
+    // methods
+    draw: function () {
+        console.log("draw");
+    }
+};
