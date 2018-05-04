389db64725e32c4fed8c9f9ed31a4a4f390aa69d
SebastianGaud <sebastiano.gaudeano@live.com>
2018-05-04 16:33:54 +0200

    add comment

--- third_section/4/property-descriptor.js
+++ third_section/4/property-descriptor.js
@@ -2,10 +2,10 @@ let person = {
     name: 'Seba'
 };
 
-const objectBase = Object.getPrototypeOf(person);
-const propDescriptor = Object.getOwnPropertyDescriptor(objectBase, 'toString');
+// const objectBase = Object.getPrototypeOf(person);
+// const propDescriptor = Object.getOwnPropertyDescriptor(objectBase, 'toString');
 
-console.log(propDescriptor);
+// console.log(propDescriptor);
 /*
     [Output] -> 
     { 
@@ -15,4 +15,12 @@ console.log(propDescriptor);
         configurable: true -> We can delete member if we want to
     }
 
-*/ 
+*/
+
+// We can define info about the property writable/enumerable/configurable are special property of a property
+// By default every property is set to true
+Object.defineProperty(person, 'name', {
+    writable: false,
+    enumerable: false,
+    configurable: true
+});
